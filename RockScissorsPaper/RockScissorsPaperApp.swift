cb999a2618ef38735226c55d190542077321aeec
Ivan <ivantuly@gmail.com>
2021-09-10 10:14:01 +0300

    Initial Commit

---
+++ RockScissorsPaper/RockScissorsPaperApp.swift
@@ -0,0 +1,17 @@
+//
+//  RockScissorsPaperApp.swift
+//  RockScissorsPaper
+//
+//  Created by mac on 10.09.2021.
+//
+
+import SwiftUI
+
+@main
+struct RockScissorsPaperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
